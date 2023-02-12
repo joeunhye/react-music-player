6524c6d29aefa2788390eb91b9a59255e93fd60e
joeunhye <masimel33@gmail.com>
2023-02-12 23:25:41 +0900

    react music player

---
+++ src/components/PlayList/PlayListItem.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function PlayListItem() {
+	return <div>PlayListItem</div>;
+}
---
+++ src/components/ProgressArea/ProgressArea.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ProgressArea() {
+	return <div>ProgressArea</div>;
+}
